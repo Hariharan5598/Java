b9053b38b307a5cef15541ffaaac6705081928fe
Hariharan5598 <39963510+Hariharan5598@users.noreply.github.com>
2018-06-26 09:57:49 +0530

    Update and rename Odd.java to Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Reverse
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        StringBuffer sb=new StringBuffer();
+        sb.append(sc.nextLine());
+        System.out.println(sb.reverse());
+    }
+}
