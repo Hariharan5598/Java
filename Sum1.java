e3c281ada21c18e9ef775172b1bc8a12b3ba1f52
Hariharan5598 <39963510+Hariharan5598@users.noreply.github.com>
2018-08-27 19:00:40 +0530

    Update and rename Pos.java to Sum1.java

--- Sum1.java
+++ Sum1.java
@@ -1,18 +1,19 @@
 import java.util.*;
-class Pos
+class Sum1
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n1=sc.nextInt();
-        int n2=sc.nextInt();
         int a[]=new int[n1];
+        int sum=0;
         for(int i=0;i<n1;i++)
         {
             a[i]=sc.nextInt();
+            sum=sum+a[i];
         }
-        Arrays.sort(a);
-        System.out.print(a[n1-n2]);
+        System.out.print(sum);
+    
     }
 }
      
